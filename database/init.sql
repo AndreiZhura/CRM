15b2050d5f3625d52e71ad9b725a09c4c213a3c6
AndreiZhura <zhura2296@yandex.ru>
2026-02-09 13:37:10 +0300

    рефакторинг кода

---
+++ database/init.sql
@@ -0,0 +1,5 @@
+-- Полная инициализация CRM
+CREATE TABLE IF NOT EXISTS clients (...);
+CREATE TABLE IF NOT EXISTS orders (...);
+CREATE TABLE IF NOT EXISTS installers (...);
+CREATE TABLE IF NOT EXISTS comments (...);
